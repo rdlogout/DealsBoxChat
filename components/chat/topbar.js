4d11f790bbc144ac395ce0ccabbf61441fb7234e
rdlogout <logoutrd@hotmail.com>
2022-03-21 21:07:03 +0530

    chat-update

--- components/chat/topbar.js
+++ components/chat/topbar.js
@@ -1,12 +1,21 @@
 import { Col, Container, Row } from "react-bootstrap";
+import { BiArrowBack } from "react-icons/bi";
+import Link from "next/link";
 
 export default ({ title }) => {
   return (
     <div id="top-bar" className="chat-title py-4 px-5">
       <Container>
         <Row>
+          <Col sm="1">
+            <Link href="/">
+              <div role="button">
+                <BiArrowBack />
+              </div>
+            </Link>
+          </Col>
           <Col sm="11">
-            <p className="mb-0 text-dark fw-bold fs-6 text-truncate">
+            <p className="mb-0 text-dark fw-bold fs-6 text-truncate text-capitalize">
               {title || "Loading ...."}
             </p>
           </Col>
--- utils/static.js
+++ utils/static.js
@@ -2,6 +2,6 @@ export const Blob_Base_Url = "https://ik.imagekit.io/up7g4bvjpofx";
 export const ImageKit_Product_uri = "https://ik.imagekit.io/up7g4bvjpofx/";
 
 export const Base_Uri =
-  process.env.NODE_ENV === "development"
+  (process.env.NODE_ENV === "development") & false
     ? "https://localhost:5001"
     : "https://socket.dealsonopenbox.com";
